409abfce4d1f814ab1b1cb5dd98fae201a77418a
willyans jhonson <jhonsonwillyans@gmail.com>
2022-06-25 23:36:57 -0300

    ✨ finalized page-about

--- wp-content/themes/WillSite/page-about.php
+++ wp-content/themes/WillSite/page-about.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
+   <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>"
+    width="<?php echo get_custom_header()->width; ?>" alt="" />
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
@@ -19,10 +21,78 @@
                                             echo wp_get_attachment_image( $image, $size );
                                     } ?>
                                     <h3>Willyans Jhonson</h3>
+                                    <div class="text-about-me">
+                                        <?php the_field('me_description'); ?>
+                                    </div>
+                                    <h3>Nossas linguagens</h3>
+                                    <div class="me-linguagens">
+                                        <?php 
+                                            $image = get_field('linguagens_img_1');
+                                            $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                                            if( $image ) {
+                                                echo wp_get_attachment_image( $image, $size );
+                                        } ?>
+                                        <?php 
+                                        $image = get_field('linguagens_img_2');
+                                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                                        if( $image ) {
+                                            echo wp_get_attachment_image( $image, $size );
+                                        } ?>
+                                        <?php 
+                                        $image = get_field('linguagens_img_3');
+                                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                                        if( $image ) {
+                                            echo wp_get_attachment_image( $image, $size );
+                                        } ?>
+                                        <?php 
+                                            $image = get_field('linguagens_img_4');
+                                            $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                                            if( $image ) {
+                                                echo wp_get_attachment_image( $image, $size );
+                                        } ?>
+                                        <?php 
+                                        $image = get_field('linguagens_img_5');
+                                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                                        if( $image ) {
+                                            echo wp_get_attachment_image( $image, $size );
+                                        } ?>
+                                        <?php 
+                                        $image = get_field('linguagens_img_6');
+                                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                                        if( $image ) {
+                                            echo wp_get_attachment_image( $image, $size );
+                                        } ?>                                  
+                                    </div>
+                                    <h3>Contact</h2>
+                                    <div class="contact">
+                                        <ul>
+                                            <a href="<?php the_field('url_contact_1'); ?>"> <?php 
+                                        $image = get_field('contact_img_1');
+                                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                                        if( $image ) {
+                                            echo wp_get_attachment_image( $image, $size );
+                                    } ?></a>
+                                            <a href="<?php the_field('url_contact_2'); ?>"> <?php 
+                                        $image = get_field('contact_img_2');
+                                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                                        if( $image ) {
+                                            echo wp_get_attachment_image( $image, $size );
+                                    } ?></a>
+                                            <a href="<?php the_field('url_contact_3'); ?>"> <?php 
+                                        $image = get_field('contact_img_3');
+                                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                                        if( $image ) {
+                                            echo wp_get_attachment_image( $image, $size );
+                                    } ?></a>
+                                        </ul>
+                                    </div>
                                     </div>
                                 </div>
                                 <div  class="about-content-2">
                                     <h2>About Us</h2>
+                                    <div class="about-us-content">
+                                        <?php the_field('about_us'); ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- wp-content/themes/WillSite/page-home.php
+++ wp-content/themes/WillSite/page-home.php
@@ -3,13 +3,8 @@
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
                     <section class="hero">
-                    <?php 
-                        $image = get_field('img_init');
-                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
-                        if( $image ) {
-                            echo wp_get_attachment_image( $image, $size );
-                        } ?>
-                    </section>
+                    
+                    
                     <section class="services">
                         <div class="services-title">
                         <h2>what are we?</h2>
@@ -20,8 +15,8 @@
                                 <?php 
                                    if(is_active_sidebar('service-1')){
                                     dynamic_sidebar('service-1');
-                                   }
-                                ?>
+                                   }?>
+                                   <a href="<?php the_permalink($post=70); ?>" title="<?php the_title_attribute(); ?>"><h3 class="h3-slide"><?php the_category(); ?></h3></a>
                             </div>
                             <div class="services-item">
                                 <?php 
@@ -29,6 +24,7 @@
                                     dynamic_sidebar('service-2');
                                    }
                                 ?>
+                                <a href="<?php the_permalink($post=67); ?>" title="<?php the_title_attribute(); ?>"><h3 class="h3-slide"><?php the_category(); ?></h3></a>
                             </div>
                             <div class="services-item">
                                 <?php 
@@ -36,11 +32,12 @@
                                     dynamic_sidebar('service-3');
                                    }
                                 ?>
+                                 <a href="<?php the_permalink($post=64); ?>" title="<?php the_title_attribute(); ?>"><h3 class="h3-slide"><?php the_category(); ?></h3></a>
                             </div>
                         </div>
                     </section>
                     <section class="home-blog">
-                        <h2>Latests Posts</h2>
+                        <h2 class="latests-posts">Latests Posts</h2>
                         <div class="container">
                             <div class="home-items">
                             <?php
@@ -58,7 +55,7 @@
                                 ?>
                                    <article>
                                        <h2><?php the_title();?></h2>
-                                       <?php the_post_thumbnail('larger');?>
+                                       <?php the_post_thumbnail('full');?>
                                        <div class="meta-info">
                                        </div>
                                        <?php the_excerpt();?>
@@ -100,7 +97,7 @@
                                         " alt="imagem 2">
                                     </div>
                                     <div class="slide">
-                                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><h3 class="h3-slide"><?php echo get_the_title(); ?></h3></a>
+                                        <a href="<?php the_permalink($post=64); ?>" title="<?php the_title_attribute(); ?>"><h3 class="h3-slide"><?php echo get_the_title(); ?></h3></a>
                                         <p class="card-text post-catgory"><small class="text-muted">Category:<?php the_category(','); ?></small></p>
                                         <p class="card-text post-tag"><small class="text-muted">Tags:<?php the_tags(' '); ?></small></p>
                                         <img src="
@@ -109,7 +106,7 @@
                                         "alt="imagem 3">
                                     </div>
                                     <div class="slide">
-                                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><h3 class="h3-slide"><?php echo get_the_title(); ?></h3></a>
+                                        <a href="<?php the_permalink($post=67); ?>" title="<?php the_title_attribute(); ?>"><h3 class="h3-slide"><?php echo get_the_title(); ?></h3></a>
                                         <p class="card-text post-catgory"><small class="text-muted">Category:<?php the_category(','); ?></small></p>
                                         <p class="card-text post-tag"><small class="text-muted">Tags:<?php the_tags(' '); ?></small></p>
                                         <img src="
@@ -135,6 +132,24 @@
                                 </div>
                             </div>
                         </div>  
+                    </section>
+                    <section class="home-ending">
+                        <div class="container">
+                        <div class="home-ending-content-1">
+                            <?php the_post_thumbnail($post=64);?>
+                        </div>
+                        <div class="home-ending-content-2">
+                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><h3 class="h3-slide"><?php the_title(); ?></h3></a>
+                        <h2>*Um post explicando nossa entrada no mercado da tecnologia</h2>
+                        <h3>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', 
+                            making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, 
+                            and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+                             Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</h3>
+                        <p><strong>Written by:</strong> <?php the_author();?></p>
+                        <p><strong>written in:</strong> <?php the_date();?></p>
+                        </div>
+                        </div>
                     </section>
                 </main>
 
